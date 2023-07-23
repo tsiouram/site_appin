e880cbcd01bc85849358147ffc74095699894572
tsiouram <tsiouram@gmail.com>
2023-07-23 12:48:27 +0300

    bla

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/site_appin/',
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
